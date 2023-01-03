fa2e3d04ec560ab6a7afbd28c328bf38c235609d
Michael La <Michael.la@stonybrook.edu>
2023-01-03 01:29:42 -0800

    working save file and open file

--- text_editor.py
+++ text_editor.py
@@ -1,21 +1,32 @@
 import tkinter as tk
-from tkinter import filedialog, Frame, Menu, Toplevel, Label, Text, BOTH
+from tkinter import filedialog, Frame, Menu, Toplevel, Label, Text, BOTH, INSERT, END
+
+current_working_file = ""
 
 #root window
 main_window = tk.Tk()
 
 #sets the initial dimensions of the window
-main_window .geometry("1280x720")
+main_window.geometry("1280x720")
 
 #title of the program
-main_window .title("The Text Editor")
+main_window.title("The Text Editor")
 
 #opens a file dialog
 def UploadAction(event=None):
-    filename = filedialog.askopenfilename()
-    file = open(filename,'r')
-    print(file.read())
-    file.close()
+    global current_working_file
+    current_working_file = filedialog.askopenfilename()
+    print(current_working_file)
+    with open(current_working_file,'r') as f:
+        text.insert(INSERT, f.read())
+        f.close()
+
+
+#writes to the current working file
+def SaveFile():
+    with open(current_working_file,'w') as f:
+        f.write(text.get(1.0, END))
+        f.close()
 
 #Version Popup 
 def Version():
@@ -33,16 +44,16 @@ def About():
     Label(top, text="Text Editor is a lightweight minimalistic text editor").place(x=25,y=100)
     Label(top, text="written in Python with the Tkinter library").place(x=25,y=120)
 
-
 #creates a menu bar
 menubar = Menu(main_window)
 main_window.config(menu=menubar)
 
+#file menu options
 filemenu = Menu(menubar)
 
 filemenu.add_command(label='New')
 filemenu.add_command(label='Open', command=UploadAction)
-filemenu.add_command(label='Save')
+filemenu.add_command(label='Save', command=SaveFile)
 filemenu.add_command(label='Save As')
 
 #adds a separator 
@@ -61,6 +72,24 @@ helpmenu.add_command(label='About', command=About)
 #adds the helpmenu to the menubar
 menubar.add_cascade(label='Help',menu=helpmenu)
 
+#edit menu options
+editmenu = Menu(menubar)
+
+editmenu.add_command(label='Undo')
+editmenu.add_command(label='Redo')
+
+editmenu.add_separator()
+
+editmenu.add_command(label='Cut')
+editmenu.add_command(label='Copy')
+editmenu.add_command(label='Paste')
+
+editmenu.add_separator()
+
+editmenu.add_command(label='Find')
+editmenu.add_command(label='Replace')
+
+menubar.add_cascade(label='Edit', menu=editmenu)
 
 text = Text(main_window)
 text.pack(expand=True, fill=BOTH)
