c900a9c9aeca97d3caed248d6c008d552f819191
Michael La <Michael.la@stonybrook.edu>
2023-01-24 15:33:51 -0500

    added ability to detect line numbers

--- text_editor.py
+++ text_editor.py
@@ -1,18 +1,31 @@
 import tkinter as tk
 from tkinter import filedialog, Frame, Menu, Toplevel, Label, Text, BOTH, INSERT, END, filedialog, Button, messagebox
 
+from time import strftime
+
 current_working_file = ""
 
 global clipboard 
 
 global curser_pos
 
+curser_pos = 0
 
 #root window
 main_window = tk.Tk()
 
 text = Text(main_window, undo=True)
+
 text.pack(expand=True, fill=BOTH)
+
+
+current_time = Label(main_window, background='black', foreground='white') 
+
+current_time.pack(side='left')
+
+
+line_label = Label(main_window, text=str(int(curser_pos)))
+line_label.pack(side='right')
 
 #sets the initial dimensions of the window
 main_window.geometry("1280x720")
@@ -20,15 +33,24 @@ main_window.geometry("1280x720")
 #title of the program
 main_window.title("The Text Editor")
 
+def time():
+    string = strftime('%H:%M:%S %p')
+    current_time.config(text=string)
+    current_time.after(1000, time)
+
+time()
+
 #opens a file dialog
+#and reads selected file
 def UploadAction(event=None):
     global current_working_file
     current_working_file = filedialog.askopenfilename()
     print(current_working_file)
     with open(current_working_file,'r') as f:
         text.insert(INSERT, f.read())
+        lines = int(text.index('end-1c').split('.')[0]) 
+        line_label.config(text= str(lines) + " lines")
         f.close()
-
 
 #writes to the current working file
 def SaveFile():
@@ -74,6 +96,7 @@ def CopyText():
     content = text.selection_get()
     clipboard = content
     curser_pos = text.index(INSERT)
+    line_label.config(text="line " + curser_pos)
 
 #sets the current selected text to the clipboard
 #deletes the selection and then sets the cursor pos to the last text index
@@ -84,6 +107,8 @@ def CutText():
     clipboard = content
     text.delete('sel.first','sel.last')
     curser_pos = text.index(INSERT)
+    line_label.config(text="line " + curser_pos)
+
 
 #pastes the text from the clipboard to the cursor position,
 #which is the last place the user clicks within the text widget
@@ -92,11 +117,13 @@ def PasteText():
     global curser_pos
     text.insert(curser_pos, clipboard)
 
-
+#every cursor click shows the current position
 def checkpos(event):
     global curser_pos
     curser_pos = text.index(INSERT)
     print(text.index(INSERT))
+    line_label.config(text= "line " + curser_pos)
+
 
 #using tkinter's built in stack undoes the most recent thing on the stack
 def undo():
@@ -125,15 +152,21 @@ def search(input_text):
         text.tag_config("start", background= "white", foreground= "black")
         #specifies the color of the background and foreground of the highlighted word
 
+#finds and replaces all instances of that word
 def find_and_replace(find, replace):
     pos = '1.0'
     while True:
+        #gets the index of the find word
         index_find_word = text.search(find, pos, END)
         if not index_find_word:
             break
+        #gets the index pos with the replaced word
         pos = '{}+{}c'.format(index_find_word, len(replace))
         print(pos)
+        #gets the ending index of the finding word
         ending_idx = '{}+{}c'.format(index_find_word, len(find))
+        #from the starting and ending index of the find word, 
+        # and replaces it with the replace word
         text.replace(index_find_word, ending_idx, replace)
         
 
@@ -176,6 +209,12 @@ def Replace():
     button.pack()
     text_to_find.pack()
     text_to_replace.pack()
+
+def pos():
+    try:
+        print(curser_pos)
+    except:
+        print("Cursor Position is not defined")
 
 text.bindtags(('Text','track-mouse-pos', '.','all'))
 text.bind_class('track-mouse-pos', '<KeyPress>', checkpos)
@@ -227,5 +266,12 @@ editmenu.add_command(label='Find', command=FindText)
 editmenu.add_command(label='Replace', command=Replace)
 
 menubar.add_cascade(label='Edit', menu=editmenu)
+
+#view menu options
+viewmenu = Menu(menubar)
+
+viewmenu.add_command(label='Show Lines', command=pos)
+
+menubar.add_cascade(label='View', menu=viewmenu)
 
 main_window.mainloop()
